0589f5d694a9a1e88c6cd5239d835d25a74a5a01
Eric Johnson <eric@monpro.net>
2016-08-27 22:43:51 -0500

    testing
    just testing parse and github

---
+++ ericjohnson1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ericjohnson1
+//
+//  Created by Stacie Johnson on 8/27/16.
+//  Copyright © 2016 Eric Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
